0f09a30c3ab0cb7a5c50462699ef38ea3b0bc68a
basil884 <ashrafbasil6@gmail.com>
2026-03-17 03:22:01 +0200

    .

--- lib/sessions/session1.dart
+++ lib/sessions/session1.dart
@@ -10,110 +10,102 @@ class CardScreen extends StatelessWidget {
       body: Column(
         mainAxisAlignment: .center,
         children: [
-          GestureDetector(
-            onTap: () => Navigator.push<void>(
-              context,
-              MaterialPageRoute<void>(
-                builder: (BuildContext context) => const SplitScreen(),
-              ),
-            ),
-            child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 10),
-              width: double.infinity,
-              height: 600,
-              color: Color(0xffBBDEFA),
-              child: Column(
-                mainAxisAlignment: .spaceEvenly,
-                children: [
-                  Row(
-                    mainAxisAlignment: .spaceEvenly,
-                    children: [
-                      ContainerWidgetChar(color: Color(0xffEE534F), text: 'A'),
-                      ContainerWidgetChar(color: Color(0xffFFA827), text: 'B'),
-                      ContainerWidgetChar(color: Color(0xffFDD735), text: 'c'),
-                    ],
-                  ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 10),
+            width: double.infinity,
+            height: 600,
+            color: Color(0xffBBDEFA),
+            child: Column(
+              mainAxisAlignment: .spaceEvenly,
+              children: [
+                Row(
+                  mainAxisAlignment: .spaceEvenly,
+                  children: [
+                    ContainerWidgetChar(color: Color(0xffEE534F), text: 'A'),
+                    ContainerWidgetChar(color: Color(0xffFFA827), text: 'B'),
+                    ContainerWidgetChar(color: Color(0xffFDD735), text: 'c'),
+                  ],
+                ),
 
-                  ///
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 80),
-                    color: Color(0xffC5CAE8),
-                    width: 350,
-                    height: 200,
-                    child: Column(
-                      mainAxisAlignment: .spaceAround,
-                      children: [
-                        Text(
-                          'Fancy Section',
-                          style: TextStyle(
-                            color: Color(0xff4250AE),
-                            fontSize: 20,
-                          ),
-                        ),
-
-                        Row(
-                          mainAxisAlignment: .spaceAround,
-                          children: [
-                            ContainerWidgetNumber(
-                              color: Color(0xff7E57C2),
-                              text: '1',
-                            ),
-                            ContainerWidgetNumber(
-                              color: Color(0xffAA47BC),
-                              text: '2',
-                            ),
-                            ContainerWidgetNumber(
-                              color: Color(0xff9675CE),
-                              text: '3',
-                            ),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: .spaceAround,
-                          children: [
-                            ContainerWidgetNumber(
-                              color: Color(0xffB968C7),
-                              text: '4',
-                            ),
-                            ContainerWidgetNumber(
-                              color: Color(0xffB39DDB),
-                              text: '5',
-                            ),
-                            ContainerWidgetNumber(
-                              color: Color(0xffCF93D9),
-                              text: '6',
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  Text(
-                    'Info Cards',
-                    style: TextStyle(color: Color(0xff4054A8), fontSize: 25),
-                  ),
-                  Row(
+                ///
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 80),
+                  color: Color(0xffC5CAE8),
+                  width: 350,
+                  height: 200,
+                  child: Column(
                     mainAxisAlignment: .spaceAround,
                     children: [
-                      CardsInfo(
-                        color: Color(0xff3BA099),
-                        number: 23,
-                        text: 'Active',
+                      Text(
+                        'Fancy Section',
+                        style: TextStyle(
+                          color: Color(0xff4250AE),
+                          fontSize: 20,
+                        ),
                       ),
-                      CardsInfo(
-                        color: Color(0xffFDAC19),
-                        number: 15,
-                        text: 'pending',
+
+                      Row(
+                        mainAxisAlignment: .spaceAround,
+                        children: [
+                          ContainerWidgetNumber(
+                            color: Color(0xff7E57C2),
+                            text: '1',
+                          ),
+                          ContainerWidgetNumber(
+                            color: Color(0xffAA47BC),
+                            text: '2',
+                          ),
+                          ContainerWidgetNumber(
+                            color: Color(0xff9675CE),
+                            text: '3',
+                          ),
+                        ],
                       ),
-                      CardsInfo(
-                        color: Color(0xff49A14D),
-                        number: 7,
-                        text: 'completed',
+                      Row(
+                        mainAxisAlignment: .spaceAround,
+                        children: [
+                          ContainerWidgetNumber(
+                            color: Color(0xffB968C7),
+                            text: '4',
+                          ),
+                          ContainerWidgetNumber(
+                            color: Color(0xffB39DDB),
+                            text: '5',
+                          ),
+                          ContainerWidgetNumber(
+                            color: Color(0xffCF93D9),
+                            text: '6',
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                ],
-              ),
+                ),
+                Text(
+                  'Info Cards',
+                  style: TextStyle(color: Color(0xff4054A8), fontSize: 25),
+                ),
+                Row(
+                  mainAxisAlignment: .spaceAround,
+                  children: [
+                    CardsInfo(
+                      color: Color(0xff3BA099),
+                      number: 23,
+                      text: 'Active',
+                    ),
+                    CardsInfo(
+                      color: Color(0xffFDAC19),
+                      number: 15,
+                      text: 'pending',
+                    ),
+                    CardsInfo(
+                      color: Color(0xff49A14D),
+                      number: 7,
+                      text: 'completed',
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
         ],
